496ea5370767021fa7104a1ce01f99c49bfa02a6
segye <tprpek@gmail.com>
2024-11-20 17:46:29 +0900

    12단계 수정

--- src/test/java/controller/WiseSayingControllerTest.java
+++ src/test/java/controller/WiseSayingControllerTest.java
@@ -54,10 +54,10 @@ class WiseSayingControllerTest {
                 .contains("작가 :")
                 .contains("1번 명언이 등록되었습니다.");
 
-        File addedFile = new File(dir + "/1.json");
-        assertThat(addedFile.exists()).isTrue();
+        File file = new File(dir + "/1.json");
+        assertThat(file.exists()).isTrue();
 
-        String content = new String(Files.readAllBytes(addedFile.toPath()));
+        String content = new String(Files.readAllBytes(file.toPath()));
         assertThat(content).contains("message\": \"현재를 사랑하라.");
         assertThat(content).contains("author\": \"작자미상");
     }
@@ -82,6 +82,7 @@ class WiseSayingControllerTest {
                 .contains("2 / 홍길동 / 현재를 사랑하라.")
                 .contains("1 / 작자미상 / 과거에 집착하지 마라.")
                 .endsWith("명령) ");
+
     }
 
     @Test
@@ -122,6 +123,13 @@ class WiseSayingControllerTest {
                 .contains("명언(기존) : 과거에 집착하지 마라.")
                 .contains("작가(기존) : 작자미상")
                 .contains("2번 명언은 존재하지 않습니다.");
+
+        File file = new File(dir + "/1.json");
+        assertThat(file.exists()).isTrue();
+
+        String content = new String(Files.readAllBytes(file.toPath()));
+        assertThat(content).contains("message\": \"현재와 자신을 사랑하라.");
+        assertThat(content).contains("author\": \"홍길동");
     }
 
     @Test
