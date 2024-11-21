e102d42dabb0be894f8a8951bdfc18f4cd3b313a
segye <tprpek@gmail.com>
2024-11-21 17:50:35 +0900

    테스트 코드 수정 및 gitignore 수정

--- src/test/java/controller/WiseSayingControllerTest.java
+++ src/test/java/controller/WiseSayingControllerTest.java
@@ -41,7 +41,12 @@ class WiseSayingControllerTest {
     @Test
     @DisplayName("등록 테스트")
     public void addTest() throws IOException {
-        String input = "등록\n현재를 사랑하라.\n작자미상\n종료\n";
+        String input = """
+                       등록
+                       현재를 사랑하라.
+                       작자미상
+                       종료
+                       """;
 
         scanner = TestUtil.genScanner(input);
 
@@ -64,7 +69,16 @@ class WiseSayingControllerTest {
     @Test
     @DisplayName("목록 테스트")
     public void listTest() throws IOException {
-        String input = "등록\n과거에 집착하지 마라.\n작자미상\n등록\n현재를 사랑하라.\n홍길동\n목록\n종료\n";
+        String input = """
+           등록
+           과거에 집착하지 마라.
+           작자미상
+           등록
+           현재를 사랑하라.
+           홍길동
+           목록
+           종료
+           """;
 
         // Scanner 객체 생성
         scanner = TestUtil.genScanner(input);
@@ -88,7 +102,13 @@ class WiseSayingControllerTest {
     @Test
     @DisplayName("삭제 테스트")
     public void deleteTest() throws IOException {
-        String input = "등록\n과거에 집착하지 마라.\n작자미상\n삭제?id=1\n삭제?id=2\n종료\n";
+        String input = """
+        등록
+        과거에 집착하지 마라.
+        작자미상
+        삭제?id=1
+        삭제?id=2
+        종료""";
 
         scanner = TestUtil.genScanner(input);
 
@@ -109,7 +129,15 @@ class WiseSayingControllerTest {
     @Test
     @DisplayName("수정 테스트")
     public void updateTest() throws IOException {
-        String input = "등록\n과거에 집착하지 마라.\n작자미상\n수정?id=1\n현재와 자신을 사랑하라.\n홍길동\n수정?id=2\n종료\n";
+        String input = """
+        등록
+        과거에 집착하지 마라.
+        작자미상
+        수정?id=1
+        현재와 자신을 사랑하라.
+        홍길동
+        수정?id=2
+        종료""";
 
         scanner = TestUtil.genScanner(input);
 
@@ -135,7 +163,12 @@ class WiseSayingControllerTest {
     @Test
     @DisplayName("빌드 테스트")
     public void buildTest() throws IOException {
-        String input = "등록\n현재를 사랑하라.\n작자미상\n빌드\n종료\n";
+        String input = """
+        등록
+        현재를 사랑하라.
+        작자미상
+        빌드
+        종료""";
 
         scanner = TestUtil.genScanner(input);
 
@@ -156,6 +189,5 @@ class WiseSayingControllerTest {
         assertThat(content).contains("message\": \"현재를 사랑하라.");
         assertThat(content).contains("author\": \"작자미상");
     }
-
 
 }
