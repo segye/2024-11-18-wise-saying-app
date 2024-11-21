a81d617ef79c9e0f8d92bd3a79f114ea4ad60674
segye <tprpek@gmail.com>
2024-11-21 17:43:29 +0900

    controller method 분리

--- src/main/java/controller/WiseSayingController.java
+++ src/main/java/controller/WiseSayingController.java
@@ -20,114 +20,149 @@ public class WiseSayingController {
             System.out.print("명령) ");
             String command = scanner.nextLine();
             if (command.equals("등록")) {
-                System.out.print("명언 : ");
-                String message = scanner.nextLine();
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-                int id = service.add(message, author);
-                System.out.println(id + "번 명언이 등록되었습니다.");
+                add();
             } else if (command.startsWith("목록")) {
-                int page = 1;
-                int size = 5;
-                String type = null;
-                String keyword = null;
-
-                if (command.contains("?")) {
-                    String[] strings = command.split("\\?");
-                    String param = strings[1];
-                    String[] params = param.split("&");
-
-                    for (String s : params) {
-                        String[] values = s.split("=");
-                        if (values[0].equals("page")) {
-                            page = Integer.parseInt(values[1]);
-                        } else if (values[0].equals("keywordType")) {
-                            type = values[1];
-                        } else if (values[0].equals("keyword")) {
-                            keyword = values[1];
-                        }
-                    }
-
-                    List<WiseSaying> result = null;
-                    if (type != null && keyword != null) {
-                        if (type.equals("content")) {
-                            result = service.findByMessage(keyword, page, size);
-                        } else if (type.equals("author")) {
-                            result = service.findByAuthor(keyword, page, size);
-                        }
-                    } else {
-                        result = service.findAll(page, size);
-                    }
-                    int start = (page - 1) * size;
-                    int total = service.getCount();
-                    int totalPage = (int) Math.ceil((double) total / size);
-
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-                    for (WiseSaying wiseSaying : result) {
-                        System.out.println(wiseSaying.getId() + " / " + wiseSaying.getAuthor() + " / " + wiseSaying.getMessage());
-                    }
-                    System.out.println("----------------------");
-                    System.out.print("페이지 : ");
-                    for (int i = 1; i <= totalPage; i++) {
-                        if (i == page) {
-                            System.out.print("[" + i + "]");
-                        } else {
-                            System.out.print(i);
-                        }
-                        if (i != totalPage) {
-                            System.out.print(" / ");
-                        }
-                    }
-                    System.out.println();
-                } else {
-                    List<WiseSaying> list = service.findAll(page, size);
-
-                    int total = service.getCount();
-                    int totalPage = (int) Math.ceil((double) total / size);
-
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-                    for (WiseSaying wiseSaying : list) {
-                        System.out.println(wiseSaying.getId() + " / " + wiseSaying.getAuthor() + " / " + wiseSaying.getMessage());
-                    }
-                    System.out.println("----------------------");
-                    System.out.print("페이지 : ");
-                    for (int i = 1; i <= totalPage; i++) {
-                        if (i == page) {
-                            System.out.print("[" + i + "]");
-                        } else {
-                            System.out.print(i);
-                        }
-                        if (i != totalPage) {
-                            System.out.print(" / ");
-                        }
-                    }
-                }
-                System.out.println();
+                list(command);
             }
             else if (command.startsWith("삭제?id=")) {
-                int id = Integer.parseInt(command.split("=")[1]);
-                boolean result = service.delete(id);
-                if (result) {
-                    System.out.println(id + "번 명언이 삭제되었습니다.");
-                } else {
-                    System.out.println(id + "번 명언은 존재하지 않습니다.");
-                }
+                delete(command);
             } else if (command.startsWith("수정?id=")) {
-                int id = Integer.parseInt(command.split("=")[1]);
-                handleUpdate(id);
+                update(command);
             } else if (command.equals("빌드")) {
-                service.build();
-                System.out.println("data.json 파일의 내용이 갱신되었습니다.");
+                build();
             } else if (command.equals("종료")) {
-                service.build();
-                scanner.close();
+                end();
                 break;
             }
         }
         scanner.close();
     }
+
+
+    private void add() {
+        System.out.print("명언 : ");
+        String message = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+        int id = service.add(message, author);
+        System.out.println(id + "번 명언이 등록되었습니다.");
+    }
+
+    private void list(String command) {
+        int page = 1;
+        int size = 5;
+        String type = null;
+        String keyword = null;
+
+        if (command.contains("?")) {
+            String[] strings = command.split("\\?");
+            String param = strings[1];
+            String[] params = param.split("&");
+
+            for (String s : params) {
+                String[] values = s.split("=");
+                if (values[0].equals("page")) {
+                    page = Integer.parseInt(values[1]);
+                } else if (values[0].equals("keywordType")) {
+                    type = values[1];
+                } else if (values[0].equals("keyword")) {
+                    keyword = values[1];
+                }
+            }
+
+            List<WiseSaying> result = null;
+            if (type != null && keyword != null) {
+                if (type.equals("content")) {
+                    result = service.findByMessage(keyword, page, size);
+                } else if (type.equals("author")) {
+                    result = service.findByAuthor(keyword, page, size);
+                }
+            } else {
+                result = service.findAll(page, size);
+            }
+            int start = (page - 1) * size;
+            int total = service.getCount();
+            int totalPage = (int) Math.ceil((double) total / size);
+
+            System.out.println("번호 / 작가 / 명언");
+            System.out.println("----------------------");
+            for (WiseSaying wiseSaying : result) {
+                System.out.println(wiseSaying.getId() + " / " + wiseSaying.getAuthor() + " / " + wiseSaying.getMessage());
+            }
+            System.out.println("----------------------");
+            System.out.print("페이지 : ");
+            for (int i = 1; i <= totalPage; i++) {
+                if (i == page) {
+                    System.out.print("[" + i + "]");
+                } else {
+                    System.out.print(i);
+                }
+                if (i != totalPage) {
+                    System.out.print(" / ");
+                }
+            }
+            System.out.println();
+        } else {
+            List<WiseSaying> list = service.findAll(page, size);
+
+            int total = service.getCount();
+            int totalPage = (int) Math.ceil((double) total / size);
+
+            System.out.println("번호 / 작가 / 명언");
+            System.out.println("----------------------");
+            for (WiseSaying wiseSaying : list) {
+                System.out.println(wiseSaying.getId() + " / " + wiseSaying.getAuthor() + " / " + wiseSaying.getMessage());
+            }
+            System.out.println("----------------------");
+            System.out.print("페이지 : ");
+            for (int i = 1; i <= totalPage; i++) {
+                if (i == page) {
+                    System.out.print("[" + i + "]");
+                } else {
+                    System.out.print(i);
+                }
+                if (i != totalPage) {
+                    System.out.print(" / ");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    private void delete(String command) {
+        int id = Integer.parseInt(command.split("=")[1]);
+        boolean result = service.delete(id);
+        if (result) {
+            System.out.println(id + "번 명언이 삭제되었습니다.");
+        } else {
+            System.out.println(id + "번 명언은 존재하지 않습니다.");
+        }
+    }
+
+    private void update(String command) {
+        int id = Integer.parseInt(command.split("=")[1]);
+        handleUpdate(id);
+    }
+    private void build() {
+        try {
+            service.build();
+            System.out.println("data.json 파일의 내용이 갱신되었습니다.");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void end() {
+        try {
+            service.build();
+            scanner.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 
     private void handleUpdate(int id) {
         WiseSaying wiseSaying = service.findById(id);
